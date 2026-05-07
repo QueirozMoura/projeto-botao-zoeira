356efaa73d3c094de8e4ab81d85269060cbd5d3c
QueirozMoura <gqueiroz2008@gmail.com>
2026-05-07 18:45:24 -0300

    testando

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const audios = [
   { caminho: "audios/classic_hurt.mp3", legenda: "Classic Hurt" },
-  { caminho: "audios/galinha-gritando-no-telhado.mp3", legenda: "Galinha Gritando No Telhado" },
+  {
+    caminho: "audios/galinha-gritando-no-telhado.mp3",
+    legenda: "Galinha Gritando No Telhado",
+  },
   { caminho: "audios/nom-nom-nom_gPJiWn4.mp3", legenda: "Nom Nom Nom" },
   { caminho: "audios/pou-estourado_zIWCpMy.mp3", legenda: "Pou Estourado" },
-  { caminho: "audios/rojao-super-estourado.mp3", legenda: "Rojao Super Estourado" },
+  {
+    caminho: "audios/rojao-super-estourado.mp3",
+    legenda: "Rojao Super Estourado",
+  },
   { caminho: "audios/yes1.mp3", legenda: "Yes" },
   { caminho: "audios/minecraft-sheep2.mp3", legenda: "Minecraft Sheep" },
-  { caminho: "audios/hello-moto-estourado.mp3", legenda: "Hello Moto Estourado" },
-  { caminho: "audios/brutal-acabou-pro-beta-globo.mp3", legenda: "Brutal Acabou Pro Beta Globo" },
+  {
+    caminho: "audios/hello-moto-estourado.mp3",
+    legenda: "Hello Moto Estourado",
+  },
+  {
+    caminho: "audios/brutal-acabou-pro-beta-globo.mp3",
+    legenda: "Brutal Acabou Pro Beta Globo",
+  },
 ];
 
 const soundboard = document.getElementById("soundboard");
@@ -16,7 +28,7 @@ const player = document.getElementById("player");
 audios.forEach((audio, i) => {
   const item = document.createElement("div");
   item.className = "sound-item";
-
+  
   const img = document.createElement("img");
   img.src = "imagens/botao.png";
   img.alt = audio.legenda;
